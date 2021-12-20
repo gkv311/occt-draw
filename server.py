c2c20e9c49304d3c48bb53652954d74c98bf728b
Kirill Gavrilov <kirill@sview.ru>
2021-12-21 00:33:25 +0300

    Update .htaccess with CORS and COEP headers for strict isolation

--- server.py
+++ server.py
@@ -2,16 +2,20 @@
 
 # Run it from install folder with optional port argument
 from http.server import HTTPServer, SimpleHTTPRequestHandler, test
-import socketserver as SocketServer
-import logging
-
 import sys
-
 class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
-        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
-        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
-        SimpleHTTPRequestHandler.end_headers(self)
+  def end_headers (self):
+    # setup mandatory MIME types
+    self.extensions_map.update({
+      ".js": "application/javascript",
+      ".wasm": "application/wasm",
+    });
+
+    # setup CORS and COEP policies to strict isolation and allowing multi-threaded WebAssembly
+    self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+    self.send_header('Cross-Origin-Opener-Policy',   'same-origin')
+    SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 5500)
+  aPort = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
+  test(CORSRequestHandler, HTTPServer, port=aPort)
